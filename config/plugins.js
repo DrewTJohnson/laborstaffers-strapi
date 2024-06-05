9db960717cfececb2cc99ff4054028b090dea981
Drew Johnson <dtjohnson1995@gmail.com>
2024-06-04 23:30:28 -0400

    Added cloudinary support

--- config/plugins.js
+++ config/plugins.js
@@ -4,6 +4,20 @@ module.exports = ({ env }) => ({
     'gql': {
       navigationItemRelated: ['Page']
     },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
     'ezforms' :{
         config:{
           captchaProvider: {
